41d78a841d30164267d23c0baf9f7dda58ac51ee
ILdar Shamiev <shamief@yandex.ru>
2018-07-05 19:55:30 +0300

    w2 sol1 search of SCC in directed graph, grader tests passed

--- solutions/w2/acyclicity.py
+++ solutions/w2/acyclicity.py
@@ -3,38 +3,68 @@
 import sys
 
 
-def acyclic(adj):
+def acyclic(adj, reversed_adj):
+    """
+    The function of finding cycles in a directed graph, i.e. SCC
+    :param adj: list of vertices with lists of adjacent vertices
+    :param reversed_adj: list of vertices with lists of adjacent vertices
+           of reversed graph
+    :return: 0 if cycle is not found, else 1
+    """
     visited = [0 for _ in range(len(adj))]
     post_visited = []
-    explore(adj, visited, post=post_visited)
-    result = False
+    dfs(reversed_adj, visited, post_visited)
+    visited = [0 for _ in range(len(adj))]
     for v in post_visited:
-        visited = [0 for _ in range(len(adj))]
-        if not visited[v]:
-            result = explore(adj, visited, v=v, check_cycle=True)
-            if result:
-                break
-    return 1 if result else 0
+        if not visited[v] and explore(adj, visited, v=v) > 1:
+            return 1
+    return 0
 
 
-def explore(adj, visited, v=0, post=None, check_cycle=False):
+def dfs(adj, visited, post_visited):
+    """
+    Traverse all vertices in the graph
+    :param adj: list of vertices with lists of adjacent vertices
+    :param visited: list of visited vertices
+    :param post_visited: list of postorder
+    """
+    i = 0
+    while True:
+        while i < len(adj) and visited[i]:
+            i += 1
+        if i < len(adj):
+            explore(adj, visited, v=i, post=post_visited)
+            i += 1
+        else:
+            break
+
+
+def explore(adj, visited, count=0, v=0, post=None):
+    """
+    Depth search in the graph
+    :param adj: list of vertices with lists of adjacent vertices
+    :param visited: list of visited vertices
+    :param count: the number of vertices in strongly connected components
+    :param v: index of vertex
+    :param post: list of postorder
+    :return: count
+    """
     visited[v] = 1
-    result = False
+    count += 1
     for w in adj[v]:
         if not visited[w]:
-            result = explore(adj, visited, w, post, check_cycle)
-            if result:
-                break
-        elif check_cycle:
-            result = True
-            break
+            count = explore(adj, visited, count, w, post)
     if post is not None:
         postvisit(v, post)
-
-    return result
+    return count
 
 
 def postvisit(v, post):
+    """
+    Insert in postorder list
+    :param v: index of vertex
+    :param post: list of postorder vertices
+    """
     post.insert(0, v)
 
 
@@ -43,8 +73,10 @@ if __name__ == '__main__':
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
-    edges = list(zip(data[1:(2 * m):2], data[0:(2 * m):2]))
+    edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
     adj = [[] for _ in range(n)]
+    reversed_adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
-    print(acyclic(adj))
+        reversed_adj[b - 1].append(a - 1)
+    print(acyclic(adj, reversed_adj))
