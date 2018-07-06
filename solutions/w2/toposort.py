e3f06ee03db4562d7e2bf4426dbe876cd6ff5692
ILdar Shamiev <shamief@yandex.ru>
2018-07-06 14:45:54 +0300

    w2 sol2 dfs in the DAG return list of postordered vertices, grader is passed

--- solutions/w2/toposort.py
+++ solutions/w2/toposort.py
@@ -3,15 +3,49 @@
 import sys
 
 
-def dfs(adj, used, order, x):
+def dfs(adj, used, order):
     # write your code here
-    pass
+    i = 0
+    while True:
+        while i < len(adj) and used[i]:
+            i += 1
+        if i < len(adj):
+            explore(adj, used, v=i, post=order)
+            i += 1
+        else:
+            break
+
+
+def explore(adj, used, v=0, post=None):
+    """
+    Depth search in the graph
+    :param adj: list of vertices with lists of adjacent vertices
+    :param visited: list of visited vertices
+    :param v: index of vertex
+    :param post: list of postorder
+    """
+    used[v] = 1
+    for w in adj[v]:
+        if not used[w]:
+            explore(adj, used, w, post)
+    if post is not None:
+        postvisit(v, post)
+
+
+def postvisit(v, post):
+    """
+    Insert in postorder list
+    :param v: index of vertex
+    :param post: list of postorder vertices
+    """
+    post.insert(0, v)
 
 
 def toposort(adj):
     used = [0] * len(adj)
     order = []
     # write your code here
+    dfs(adj, used, order)
     return order
 
 
