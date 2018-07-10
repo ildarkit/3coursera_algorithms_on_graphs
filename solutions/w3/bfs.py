35c3adc13b508913671fa62ce4398ab579cce7d7
Ildar Shamiev <shamief@yandex.ru>
2018-07-10 17:05:15 +0300

    w3 sol1 bfs with queue implemented, grader tests passed

--- solutions/w3/bfs.py
+++ solutions/w3/bfs.py
@@ -1,12 +1,46 @@
 # Uses python3
 
 import sys
-import queue
+
+
+BIG_DIST = 100000000
 
 
 def distance(adj, s, t):
     # write your code here
-    return -1
+    prev = bfs(adj, s)
+    spath = path(s, t, prev)
+    return spath
+
+
+def bfs(adj, s):
+    dist = []
+    prev = []
+    for _ in range(len(adj)):
+        dist.append(BIG_DIST)
+        prev.append(None)
+    dist[s] = 0
+    queue = []
+    queue.insert(0, s)
+    while queue:
+        u = queue.pop()
+        for v in adj[u]:
+            if dist[v] == BIG_DIST:
+                queue.insert(0, v)
+                dist[v] = dist[u] + 1
+                prev[v] = u
+    return prev
+
+
+def path(s, t, prev):
+    result = []
+    while s != t:
+        if t is not None:
+            result.append(t)
+        else:
+            return -1
+        t = prev[t]
+    return len(result)
 
 
 if __name__ == '__main__':
