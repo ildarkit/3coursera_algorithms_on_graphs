f2205eed3812ffe91fef17fd9c60cbe899a81a1d
Ildar Shamiev <shamief@yandex.ru>
2018-07-18 11:33:52 +0300

    w4 sol1 dijkstra with a list implemented, grader tests passed

--- solutions/w4/dijkstra.py
+++ solutions/w4/dijkstra.py
@@ -4,46 +4,73 @@ import sys
 
 
 BIG_DIST = 100000000
+USED_DIST = 1000000000
 
 
 def distance(adj, cost, s, t):
     # write your code here
-    return dijkstra(adj, cost, s, t)
-    # return min_cost(s, t, prev, dist)
+    return dijkstra_list(adj, cost, s, t)
 
 
-def dijkstra(adj, cost, s, t):
+def dijkstra_list(adj, cost, s, t):
+    """
+    Implementation of Dijkstra's algorithm for finding the path
+    with the least cost of flight. For this, a list of distances was used
+    (the asymptotics of O (V ** 2)).
+    :param adj: list of adjacency
+    :param cost: list of costs of adjacency nodes
+    :param s: start node
+    :param t: end node
+    :return: distance of end node
+    """
     dist = []
-    # prev = []
-    used = []
-    for _ in range(len(adj)):
+    for i in range(len(adj)):
         dist.append(BIG_DIST)
-        # prev.append(None)
-        used.append(None)
     dist[s] = 0
-    queue = [s]
-    while queue:
-        u = queue.pop()
+    len_queue = len(adj)
+    dist2 = dist[:]
+    while len_queue:
+        # get node with min distance
+        u = dist2.index(min(dist2))
+        len_queue -= 1
+        dist2[u] = USED_DIST
         for i, v in enumerate(adj[u]):
-            if used[v] is None:
-                queue.insert(0, v)
-                used[v] = v
+            # relaxing
             if dist[v] > dist[u] + cost[u][i]:
                 dist[v] = dist[u] + cost[u][i]
-                # prev[v] = u
+                dist2[v] = dist[v]
     return dist[t] if dist[t] != BIG_DIST else -1
 
 
-# def min_cost(s, t, prev, dist):
-#     result = 0
-#     while s != t:
-#         if t is not None:
-#             if dist[t]:
-#                 result += dist[t]
-#         else:
-#             return -1
-#         t = prev[t]
-#     return result
+def naive_distance(adj, cost, s, t):
+    """
+    A naive realization of finding a path with
+    a minimum cost of a flight.
+    Calculations will occur as long as the distance varies.
+    :param adj: list of adjacency
+    :param cost: list of costs of adjacency nodes
+    :param s: start node
+    :param t: end node
+    :return: distance of end node
+    """
+    dist = [BIG_DIST for _ in range(len(adj))]
+    dist[s] = 0
+    i = s
+    is_changed = False
+    while True:
+        u = adj[i]
+        for j, v in enumerate(u):
+            if dist[v] > dist[i] + cost[i][j]:
+                is_changed = True
+                dist[v] = dist[i] + cost[i][j]
+        i += 1
+        if i == len(adj):
+            if is_changed:
+                i = 0
+                is_changed = False
+            else:
+                break
+    return dist[t] if dist[t] != BIG_DIST else -1
 
 
 if __name__ == '__main__':
@@ -60,3 +87,4 @@ if __name__ == '__main__':
         cost[a - 1].append(w)
     s, t = data[0] - 1, data[1] - 1
     print(distance(adj, cost, s, t))
+    # print(naive_distance(adj, cost, s, t))
