4864deba77c1f49efc38cbfa99ff794b13a945be
Ildar Shamiev <shamief@yandex.ru>
2018-07-19 23:09:43 +0300

    w4 sol2 implemented bellman-ford algorithm for searching negative cycles in the graph, grader tests passed

--- solutions/w4/negative_cycle.py
+++ solutions/w4/negative_cycle.py
@@ -3,9 +3,41 @@
 import sys
 
 
+BIG_DIST = 100000000
+
+
 def negative_cycle(adj, cost):
     # write your code here
-    return 0
+    return bellman_ford(adj, cost)
+
+
+def bellman_ford(adj, cost):
+    """
+    Implementation of the Bellman-Ford algorithm
+    for searching of negative cycles.
+    To do this, need to perform relaxing a k-times for all vertices.
+    There is the negative cycle in the graph
+    if the change of dist occurs at the last iteration.
+
+    :param adj: list of lists of adjacency vertices
+    :param cost: list of lists of weight
+    :return: 1 if there is negative cycles, otherwise return 0
+    """
+    dist = [BIG_DIST for _ in range(len(adj))]
+    dist[0] = 0
+    k = 1
+    while k <= len(adj):
+        is_changed = False
+        for u in range(len(adj)):
+            for i, v in enumerate(adj[u]):
+                if dist[v] > dist[u] + cost[u][i]:
+                    is_changed = True
+                    if k == len(adj):
+                        return 1
+                    dist[v] = dist[u] + cost[u][i]
+        if not is_changed:
+            return 0
+        k += 1
 
 
 if __name__ == '__main__':
